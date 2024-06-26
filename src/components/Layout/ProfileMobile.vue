2172f4ed0e0f9e2da1ee5b411edf5d6dd05b35ab
Irwanto Huang <irwanto.ng11@gmail.com>
2024-06-26 21:32:54 +0700

    Adjust style

--- src/components/Layout/ProfileMobile.vue
+++ src/components/Layout/ProfileMobile.vue
@@ -23,17 +23,16 @@ defineEmits([
 
                 <div class="container mx-auto py-[75px] h-full">
                     <div class="h-full flex flex-col items-center">
-
-                        <header>
+                        <header class="w-full flex flex-col items-center">
                             <div
-                                class="w-[150px] h-[150px] p-2 bg-primary-light border border-primary-dark-200 rounded-2xl shadow-inner overflow-hidden">
+                                class="w-[150px] h-[150px] p-2 bg-primary-light border border-primary-dark-200 rounded-2xl shadow-inner overflow-hidden flex items-center justify-center">
                                 <img :src="profile.profilePicture" :alt="profile.name"
                                     class="w-full h-full rounded-2xl object-cover">
                             </div>
 
                             <div class="mt-2 text-center">
-                                <h1 class="font-bold text-lg text-primary/75">{{ profile.name }}</h1>
-                                <p class="text-[10px] font-bold text-primary-dark-200/50">{{ profile.headline }}</p>
+                                <h1 class="font-bold text-lg md:text-2xl text-primary/75">{{ profile.name }}</h1>
+                                <p class="text-[10px] md:text-xs font-bold text-primary-dark-200/50">{{ profile.headline }}</p>
                             </div>
                         </header>
 
@@ -42,11 +41,11 @@ defineEmits([
                         <article class="flex flex-col gap-4 w-full">
                             <section id="contact">
                                 <div v-for="data in profile.contact" class="mt-6 flex items-center h-auto gap-4 ">
-                                    <Icon class="w-[45px] h-[45px] text-xl" :icon="data.icon" />
+                                    <Icon class="w-[45px] h-[45px] md:w-[55px] md:h-[55px] md:text-2xl text-xl" :icon="data.icon" />
 
                                     <div class="flex flex-col h-full justify-between ">
-                                        <h3 class="text-xs font-bold text-primary-dark-200/50 uppercase">{{ data.title }}</h3>
-                                        <a href="#" class="text-sm font-medium text-accent transition-all duration-50 hover:text-accent-200">{{ data.url }}</a>
+                                        <h3 class="text-xs md:text-sm font-bold text-primary-dark-200/50 uppercase">{{ data.title }}</h3>
+                                        <a href="#" class="text-sm md:text-base font-medium text-accent transition-all duration-50 hover:text-accent-200">{{ data.url }}</a>
                                     </div>
                                 </div>
                             </section>
@@ -54,7 +53,7 @@ defineEmits([
 
                             <section id="social-media" class="mt-6">
                                 <div class="flex flex-row items-center gap-4 justify-center">
-                                    <Icon class="w-[50px] h-[50px] text-3xl" v-for="data in profile.socialMedia" :icon="data.icon" :link="data.url" />
+                                    <Icon class="w-[50px] h-[50px] text-3xl md:w-[65px] md:h-[65px] md:text-4xl" v-for="data in profile.socialMedia" :icon="data.icon" :link="data.url" />
                                 </div>
                             </section>
                         </article>
