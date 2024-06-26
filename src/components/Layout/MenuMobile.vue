8a665533b9034420ed06123999c433bd29a3ece2
Irwanto Huang <irwanto.ng11@gmail.com>
2024-06-26 20:08:59 +0700

    Finalization Menu Section

--- src/components/Layout/MenuMobile.vue
+++ src/components/Layout/MenuMobile.vue
@@ -18,7 +18,7 @@ defineEmits([
                 class="z-[999] fixed inset-x-0 bottom-0 w-full h-[50%] flex flex-col gap-4 px-4 py-6 bg-primary-light-200 shadow-2xl">
                 <li v-for="data in menu" :key="data.id" class="flex">
                     <router-link @click="$emit('toggleMenuVisibility')" :to="{ name: data.name }" 
-                        class="flex gap-3 items-center rounded-sm py-1.5 ps-4 w-1/3 text-base font-medium hover:bg-primary hover:text-primary-light transition-all duration-100 ease-in"
+                        class="flex gap-3 items-center rounded-sm py-1.5 ps-4 w-1/2 text-base font-medium hover:bg-primary hover:text-primary-light transition-all duration-100 ease-in"
                         active-class="bg-primary text-primary-light hover:bg-accent-200">
                         <fa-icon :icon="data.icon" />
                         {{ data.name }}
--- src/data/menu.ts
+++ src/data/menu.ts
@@ -11,24 +11,24 @@ export const menu: Menu[] = [
         id: 2,
         name: "About",
         link: "/about",
-        icon: "fa-solid fa-home"
+        icon: "fa-solid fa-circle-info"
     },
     {
         id: 3,
         name: "Skill",
         link: "/skill",
-        icon: "fa-solid fa-home"
+        icon: "fa-solid fa-gear"
     },
     {
         id: 4,
         name: "Portfolio",
         link: "/portfolio",
-        icon: "fa-solid fa-home"
+        icon: "fa-solid fa-briefcase"
     },
     {
         id: 5,
         name: "Client",
         link: "/client",
-        icon: "fa-solid fa-home"
+        icon: "fa-solid fa-users"
     }
 ]
