e706e58ca6ce332a5665760b09bdccbf73e04c37
Irwanto Huang <irwanto.ng11@gmail.com>
2024-06-28 21:45:57 +0700

    FIX Skill Accessibility

--- src/components/Skill/SkillContent.vue
+++ src/components/Skill/SkillContent.vue
@@ -12,8 +12,8 @@ defineProps({
     <div class="card flex gap-3 items-start group">
         <Icon class="min-w-[50px] min-h-[50px] text-3xl" :icon="skill?.icon" :type="iconType" />
         <div class="flex flex-col gap-1">
-            <h3 class="font-semibold text-primary text-sm md:text-base">{{ skill?.name }}</h3>
-            <p class="font-medium text-primary-dark-200/50 text-xs md:text-sm">{{ skill?.level }}</p>
+            <h2 class="font-semibold text-accent-200 text-sm md:text-base">{{ skill?.name }}</h2>
+            <p class="font-medium text-primary-dark-200/90 text-xs md:text-sm">{{ skill?.level }}</p>
         </div>
     </div>
 </template>
