d674015bfdd0c7301168e5cc3def249a17c014c9
Irwanto Huang <irwanto.ng11@gmail.com>
2024-06-29 00:35:04 +0700

    Remove unused function

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
 import MainLayout from './layouts/MainLayout.vue';
-
-
-// onMounted(() => {
-//     if (localStorage.appearance === 'dark' 
-//         || (!('appearance' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-
-//         }
-// })
-
 </script>
 
 <template>
