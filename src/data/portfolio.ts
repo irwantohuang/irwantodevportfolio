878380cb9c443216d72482bece545f29f431d61c
Irwanto Huang <irwanto.ng11@gmail.com>
2024-06-28 18:52:25 +0700

    ADD Portoflio Data

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -23,9 +23,60 @@ import P2Image4 from '../assets/portfolio/academia/image-4.png'
 import P2Image5 from '../assets/portfolio/academia/image-5.png'
 
 
+// Netflix
+import Cover3 from '../assets/portfolio/clone/netflix/cover.png'
+import P3Image1 from '../assets/portfolio/clone/netflix/image-1.png'
+import P3Image2 from '../assets/portfolio/clone/netflix/image-2.png'
+import P3Image3 from '../assets/portfolio/clone/netflix/image-3.png'
+import P3Image4 from '../assets/portfolio/clone/netflix/image-4.png'
+import P3Image5 from '../assets/portfolio/clone/netflix/image-5.png'
+
+// Interno
+import Cover4 from '../assets/portfolio/interno/cover.png'
+import P4Image1 from '../assets/portfolio/interno/image-1.png'
+import P4Image2 from '../assets/portfolio/interno/image-2.png'
+import P4Image3 from '../assets/portfolio/interno/image-3.png'
+import P4Image4 from '../assets/portfolio/interno/image-4.png'
+import P4Image5 from '../assets/portfolio/interno/image-5.png'
+import P4Image6 from '../assets/portfolio/interno/image-6.png'
+import P4Image7 from '../assets/portfolio/interno/image-7.png'
+import P4Image8 from '../assets/portfolio/interno/image-8.png'
+import P4Image9 from '../assets/portfolio/interno/image-9.png'
+
+
+// Miracell
+import Cover5 from '../assets/portfolio/miracell/cover.png'
+import P5Image1 from '../assets/portfolio/miracell/image-1.png'
+import P5Image2 from '../assets/portfolio/miracell/image-2.png'
+import P5Image3 from '../assets/portfolio/miracell/image-3.png'
+import P5Image4 from '../assets/portfolio/miracell/image-4.png'
+import P5Image5 from '../assets/portfolio/miracell/image-5.png'
+
+// Portfolio 1
+import Cover6 from '../assets/portfolio/portfolio-1/cover.png'
+import P6Image1 from '../assets/portfolio/portfolio-1/image-1.png'
+import P6Image2 from '../assets/portfolio/portfolio-1/image-2.png'
+import P6Image3 from '../assets/portfolio/portfolio-1/image-3.png'
+import P6Image4 from '../assets/portfolio/portfolio-1/image-4.png'
+import P6Image5 from '../assets/portfolio/portfolio-1/image-5.png'
+import P6Image6 from '../assets/portfolio/portfolio-1/image-6.png'
+import P6Image7 from '../assets/portfolio/portfolio-1/image-7.png'
+
+// Expanding Cards
+import Cover7 from '../assets/portfolio/expanding-cards/cover.png'
+import P7Image1 from '../assets/portfolio/expanding-cards/image-1.png'
+import P7Image2 from '../assets/portfolio/expanding-cards/image-2.png'
+
+// Login Form
+import Cover8 from '../assets/portfolio/login-form/cover.png'
+import P8Image1 from '../assets/portfolio/login-form/image-1.png'
+import P8Image2 from '../assets/portfolio/login-form/image-2.png'
+import P8Image3 from '../assets/portfolio/login-form/image-3.png'
+
 export const portfolio: Portfolio[] = [
     {
         id: '7b224ccc-aac7-4ff0-8759-5ae3d370922a',
+        category: ['Web Development'],
         cover: Cover1,
         title: "Youtube Showcase",
         detail: {
@@ -37,11 +88,12 @@ export const portfolio: Portfolio[] = [
                 { title: "API Integration", tecnology: ['Google APi v3 (for fetching data from YouTube)'] }
             ],
             livePreview: "https://vue-youtube-showcase.netlify.app/",
-            githubLink: "https://github.com/irwantohuang/nihonggo-mantappu-showcase/tree/main"
+            githubLink: "https://github.com/irwantohuang/nihonggo-mantappu-showcase"
         }
     },
     {
         id: 'ddfac99c-4a50-4b37-bcc5-025bb25baeb0',
+        category: ['Web Development'],
         cover: Cover2,
         title: "Academia Connect",
         detail: {
@@ -56,30 +108,97 @@ export const portfolio: Portfolio[] = [
     },
     {
         id: '13aad3fc-bb28-45a6-9c4d-fb1a0882a1e5',
-        cover: P1Image1,
-        title: "Academia Connect",
+        category: ['Web Development', 'Cloning'],
+        cover: Cover3,
+        title: "Nerflix Clone",
         detail: {
-            image: [Cover2, P2Image1, P2Image2, P2Image3, P2Image4, P2Image5],
-            description: "",
+            image: [Cover3, P3Image1, P3Image2, P3Image3, P3Image4, P3Image5],
+            description: "Netflix Clone is a project aimed at recreating the popular streaming platform’s user interface. It allows users to browse through a library of movies and TV shows, watch trailers, and experience a seamless media streaming experience.",
             technology: [
-                { title: "Core Technologies", tecnology: ['Vue.js', 'TailwindCSS'] },
+                { title: "Core Technologies", tecnology: ['HTML', 'CSS', 'Javascript'] },
             ],
-            livePreview: "https://academiaconnect.netlify.app/",
-            githubLink: "https://github.com/irwantohuang/academia"
+            livePreview: "https://irwantohuang.github.io/netflix-clone.github.io/",
+            githubLink: "https://github.com/irwantohuang/netflix-clone.github.io"
         }
     },
     {
-        id: 'b47dac76-e216-4249-b9cf-fa3adf26f297',
-        cover: P2Image1,
-        title: "Academia Connect",
+        id: '5dbc1bb1-a576-4db6-8150-e885147c94dc',
+        category: ['Web Development'],
+        cover: Cover4,
+        title: "Interno",
         detail: {
-            image: [Cover2, P2Image1, P2Image2, P2Image3, P2Image4, P2Image5],
-            description: "",
+            image: [Cover4, P4Image1, P4Image2, P4Image3, P4Image4, P4Image5, P4Image6, P4Image7, P4Image8, P4Image9],
+            description: "A dummy landing page designed for interno. an interior design concept. the focus was on creating an aesthetically pleasing and inviting interface to captivate visitors.",
+            technology: [
+                { title: "Core Technologies", tecnology: ['HTML', 'TailwindCSS', 'Javascript'] },
+                { title: "Libraries and Framework", tecnology: ['Swiper', 'Remix Icon'] },
+
+            ],
+            livePreview: "https://internopages.netlify.app/",
+            githubLink: "https://github.com/irwantohuang/interno"
+        }
+    },
+    {
+        id: 'd1d10a41-87d3-45c2-a5ff-61607e33fceb',
+        category: ['Web Development'],
+        cover: Cover5,
+        title: "Miracell Safety",
+        detail: {
+            image: [Cover5, P5Image1, P5Image2, P5Image3, P5Image4, P5Image5 ],
+            description: "This is a landing page for Miracell Safety. an online safety shoe store. The goal was to create an attractive and responsive layout so visitors can easily explore the products offered.",
+            technology: [
+                { title: "Core Technologies", tecnology: ['HTML', 'CSS', 'Javascript'] },
+                { title: "Libraries and Framework", tecnology: ['Swiper', 'Font Awesome'] },
+
+            ],
+            livePreview: "https://irwantohuang.github.io/miracell-web/",
+            githubLink: "https://github.com/irwantohuang/miracell-web"
+        }
+    },
+    {
+        id: '4b3d2845-39ca-4153-8915-5bb0f0e57411',
+        category: ['Web Development'],
+        cover: Cover6,
+        title: "Landing Page - Portfolio",
+        detail: {
+            image: [Cover6, P6Image1, P6Image2, P6Image3, P6Image4, P6Image5, P6Image6, P6Image7 ],
+            description: "The project serves as a platform for showcasing various works and projects, featuring a light and dark mode option for user preference. The emphasis is on presenting information clearly and engagingly, ensuring an optimal user experience.",
             technology: [
                 { title: "Core Technologies", tecnology: ['Vue.js', 'TailwindCSS'] },
+                { title: "Libraries and Framework", tecnology: ['AOS', 'Font Awesome'] },
             ],
-            livePreview: "https://academiaconnect.netlify.app/",
-            githubLink: "https://github.com/irwantohuang/academia"
+            livePreview: "https://vuewithtailwind.netlify.app/",
+            githubLink: "https://github.com/irwantohuang/portfolio-vue-tailwind"
+        }
+    },
+    {
+        id: '36b696c8-dbd0-4773-b31d-c6fa8ed6f9bf',
+        category: ['UI Component'],
+        cover: Cover7,
+        title: "Expanding Cards",
+        detail: {
+            image: [Cover7, P7Image1, P7Image2],
+            description: "A minimalist project showcasing interactive cards that expand and reveal content upon clicking. Built with HTML, CSS for styling, and JavaScript for functionality.",
+            technology: [
+                { title: "Core Technologies", tecnology: ['HTML', 'CSS', 'Javascript'] },
+            ],
+            livePreview: "https://irwantohuang.github.io/expanding-cards/",
+            githubLink: "https://github.com/irwantohuang/expanding-cards"
+        }
+    },
+    {
+        id: '0da52396-f274-4cdc-89ec-727ce790cfa8',
+        category: ['UI Component'],
+        cover: Cover8,
+        title: "Login Form",
+        detail: {
+            image: [Cover8, P8Image1, P8Image2, P8Image3],
+            description: "A basic login form project demonstrating front-end development skills with HTML for structure, CSS for styling, and JavaScript for form validation and interactivity.",
+            technology: [
+                { title: "Core Technologies", tecnology: ['HTML', 'CSS', 'Javascript'] },
+            ],
+            livePreview: "https://irwantohuang.github.io/login-form/",
+            githubLink: "https://github.com/irwantohuang/login-form"
         }
     },
 ]
--- src/types/Portfolio.ts
+++ src/types/Portfolio.ts
@@ -2,6 +2,7 @@ import { PortfolioDetail } from "./PortfolioDetail";
 
 export interface Portfolio {
     id: string,
+    category: string[],
     cover: string,
     title: string,
     detail: PortfolioDetail
