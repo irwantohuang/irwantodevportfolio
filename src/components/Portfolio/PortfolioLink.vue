371d9ad2017bcd3864b2aac3c8e4c4405dcc0c5f
Irwanto Huang <irwanto.ng11@gmail.com>
2024-06-29 16:24:56 +0700

    FIX Bugs on Link Click

--- src/components/Portfolio/PortfolioLink.vue
+++ src/components/Portfolio/PortfolioLink.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed } from 'vue';
 import Icon from '../../ui/common/Icon.vue';
 import Link from '../../ui/common/Link.vue';
 
@@ -7,9 +7,9 @@ const props = defineProps({
     livePreview: String,
     github: String,
     type: String,
-
 })
-const links = ref([
+
+const links = computed(() => [
     { id: 1, name: 'Github Repository', icon: 'fa-brands fa-github', url: props.github},
     { id: 2, name: 'Live Preview', icon: 'fa-solid fa-up-right-from-square', url: props.livePreview}
 ])
@@ -24,7 +24,6 @@ const redirectTo = (url?: string) => window.open(url, "_blank")
     </div>
 
     <div v-if="type === 'desktop'" class="flex flex-col gap-2 w-full items-start mt-4">
-
         <Link v-for="link in links" :url="link.url" :class="link.id === 1 ? 'btn-primary' : 'btn-outline'">
             <slot>
                 {{ link.name }}
